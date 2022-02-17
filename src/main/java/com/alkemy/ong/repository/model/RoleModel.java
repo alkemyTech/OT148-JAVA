a13eca84a369755f60d6ba8f360b368b7a29e95b
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-17 15:24:41 -0300

    OT148-16 RoleModel created

---
+++ src/main/java/com/alkemy/ong/repository/model/RoleModel.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.repository.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "roles")
+public class RoleModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private LocalDateTime timestamps;
+}
