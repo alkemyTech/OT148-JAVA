b6cfad33cf9a584d1e5934d63420de7c0b465743
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-17 15:26:48 -0300

    OT148-16 RoleMapper created

---
+++ src/main/java/com/alkemy/ong/mapper/RoleMapper.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.mapper;
+
+import com.alkemy.ong.domain.Role;
+import com.alkemy.ong.repository.model.RoleModel;
+
+public class RoleMapper {
+
+    public static Role RoleModelToDomain(RoleModel roleModel){
+        Role roleDomain = new Role();
+        roleDomain.setName(roleModel.getName());
+        roleDomain.setDescription(roleModel.getDescription());
+        return roleDomain;
+    }
+
+    public static RoleModel RoleDomainToModel (Role roleDomain){
+        RoleModel roleModel = new RoleModel();
+        roleModel.setName(roleDomain.getName());
+        roleModel.setDescription(roleDomain.getDescription());
+        return roleModel;
+    }
+
+}
