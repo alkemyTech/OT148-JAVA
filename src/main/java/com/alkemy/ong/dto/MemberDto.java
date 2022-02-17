df87d4a704077fb6f245ecde302dcf5552e9377a
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-17 11:48:06 -0300

    add MemberDto

---
+++ src/main/java/com/alkemy/ong/dto/MemberDto.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    public String name;
+    public String facebookUrl;
+    public String instagramUrl;
+    public String linkedinUrl;
+    public String image;
+    public String description;
+}
