0c9afd7fe47956ea5d0d533958f0a8a132749fcd
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-17 11:35:27 -0300

    add member

---
+++ src/main/java/com/alkemy/ong/domain/Member.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String name;
+    private String facebookUrl;
+    private String instagramUrl;
+    private String linkedinUrl;
+    private String image;
+    private String description;
+}
