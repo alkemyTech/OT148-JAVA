1696bae03a135764e9a52f338938afd08387522a
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-17 20:28:24 -0300

    Modify MemberRepository

--- src/main/java/com/alkemy/ong/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.MemberModel;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<MemberModel,Integer> {
-    public MemberModel findByIdMember(Integer idMember);
-    public MemberModel findByName(String name);
+public interface MemberRepository extends CrudRepository<MemberModel,Integer> {
 }
