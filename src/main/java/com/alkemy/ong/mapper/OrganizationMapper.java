a90e791cbbca1553db24759cf02aaa271c8d3b6c
PabloD97 <pablo97758@gmail.com>
2022-02-17 14:45:23 -0300

    OT148-15: OrganizationMapper
    - Removed setters from both mappers
    - The builder functionality was used to set the fields

--- src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
+++ src/main/java/com/alkemy/ong/mapper/OrganizationMapper.java
@@ -6,26 +6,26 @@ import com.alkemy.ong.repository.model.OrganizationModel;
 public class OrganizationMapper {
 
     public static Organization mapModelToDomain(OrganizationModel organizationModel){
-        Organization organizationDomain = Organization.builder().build();
-        organizationDomain.setName(organizationModel.getName());
-        organizationDomain.setImage(organizationModel.getImage());
-        organizationDomain.setAddress(organizationModel.getAddress());
-        organizationDomain.setPhone(organizationModel.getPhone());
-        organizationDomain.setEmail(organizationModel.getEmail());
-        organizationDomain.setWelcomeText(organizationModel.getWelcomeText());
-        organizationDomain.setAboutUsText(organizationModel.getAboutUsText());
+        Organization organizationDomain = Organization.builder().
+                name(organizationModel.getName()).
+                image(organizationModel.getImage()).
+                address(organizationModel.getAddress()).
+                phone(organizationModel.getPhone()).
+                email(organizationModel.getEmail()).
+                welcomeText(organizationModel.getWelcomeText()).
+                aboutUsText(organizationModel.getAboutUsText()).build();
         return organizationDomain;
     }
 
     public static OrganizationModel mapDomainToModel(Organization organizationDomain){
-        OrganizationModel organizationModel = new OrganizationModel();
-        organizationModel.setName(organizationDomain.getName());
-        organizationModel.setImage(organizationDomain.getImage());
-        organizationModel.setAddress(organizationDomain.getAddress());
-        organizationModel.setPhone(organizationDomain.getPhone());
-        organizationModel.setEmail(organizationDomain.getEmail());
-        organizationModel.setWelcomeText(organizationDomain.getWelcomeText());
-        organizationModel.setAboutUsText(organizationDomain.getAboutUsText());
+        OrganizationModel organizationModel = OrganizationModel.builder().
+                name(organizationDomain.getName()).
+                image(organizationDomain.getImage()).
+                address(organizationDomain.getAddress()).
+                phone(organizationDomain.getPhone()).
+                email(organizationDomain.getEmail()).
+                welcomeText(organizationDomain.getWelcomeText()).
+                aboutUsText(organizationDomain.getAboutUsText()).build();
         return organizationModel;
     }
 
