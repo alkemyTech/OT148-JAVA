7af7290d4393c6ae777c5549d16d4515c7cafb26
PabloD97 <pablo97758@gmail.com>
2022-02-17 13:14:15 -0300

    OT148-15: OrganizationRepository
    - Changed the Intenger to Long

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrganizationRepository extends CrudRepository<OrganizationModel, Integer> {
+public interface OrganizationRepository extends CrudRepository<OrganizationModel, Long> {
 }
