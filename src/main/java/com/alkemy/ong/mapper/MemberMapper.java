8025c67aabe1eb6da2d98f92403006efeb1a6003
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-18 19:11:15 -0300

    modified points at the beginning of each line

--- src/main/java/com/alkemy/ong/mapper/MemberMapper.java
+++ src/main/java/com/alkemy/ong/mapper/MemberMapper.java
@@ -6,26 +6,26 @@ import com.alkemy.ong.repository.model.MemberModel;
 public class MemberMapper {
 
     public static Member mapModelToDomain(MemberModel memberModel){
-        Member memberDomain = Member.builder().
-                idMember(memberModel.getIdMember()).
-                name(memberModel.getName()).
-                facebookUrl(memberModel.getFacebookUrl()).
-                instagramUrl(memberModel.getInstagramUrl()).
-                linkedinUrl(memberModel.getLinkedinUrl()).
-                image(memberModel.getImage()).
-                description(memberModel.getDescription()).build();
+        Member memberDomain = Member.builder()
+                .id(memberModel.getId())
+                .name(memberModel.getName())
+                .facebookUrl(memberModel.getFacebookUrl())
+                .instagramUrl(memberModel.getInstagramUrl())
+                .linkedinUrl(memberModel.getLinkedinUrl())
+                .image(memberModel.getImage())
+                .description(memberModel.getDescription()).build();
         return memberDomain;
     }
 
     public static MemberModel mapDomainToModel(Member memberDomain){
-        MemberModel memberModel = MemberModel.builder().
-                idMember(memberDomain.getIdMember()).
-                name(memberDomain.getName()).
-                facebookUrl(memberDomain.getFacebookUrl()).
-                instagramUrl(memberDomain.getInstagramUrl()).
-                linkedinUrl(memberDomain.getLinkedinUrl()).
-                image(memberDomain.getImage()).
-                description(memberDomain.getDescription()).build();
+        MemberModel memberModel = MemberModel.builder()
+                .id(memberDomain.getId())
+                .name(memberDomain.getName())
+                .facebookUrl(memberDomain.getFacebookUrl())
+                .instagramUrl(memberDomain.getInstagramUrl())
+                .linkedinUrl(memberDomain.getLinkedinUrl())
+                .image(memberDomain.getImage())
+                .description(memberDomain.getDescription()).build();
         return memberModel;
     }
 }
