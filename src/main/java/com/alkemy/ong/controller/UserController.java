c30d789170631d288ab769d198ce8fb7efed3835
PabloD97 <pablo97758@gmail.com>
2022-02-18 11:37:21 -0300

    OT148-24: UserController
    - Added the handleValidationException() method to catch exception

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -6,11 +6,15 @@ import com.alkemy.ong.mapper.UserMapper;
 import com.alkemy.ong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -26,17 +30,21 @@ public class UserController {
     private UserMapper mapper = new UserMapper();
 
     @PostMapping("/register")
-    public ResponseEntity<?> userRegister(@Valid @RequestBody UserDTO userDto, BindingResult result){
-        Map<String, Object> validations = new HashMap<>();
-        if(result.hasErrors()){
-            result.getFieldErrors().
-                    forEach(error -> validations.put(error.getField(),error.getDefaultMessage()));
-            return ResponseEntity.badRequest().body(validations);
-        }
+    public ResponseEntity<?> userRegister(@Valid @RequestBody UserDTO userDto){
         User userDomain = mapper.mapDtoToDomain(userDto);
         return ResponseEntity.ok(userService.registerUser(userDomain));
     }
 
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
 }
