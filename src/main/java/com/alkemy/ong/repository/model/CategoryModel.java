37340d3133226fb585541563380580ba4d8d5114
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-18 11:42:26 -0300

    Adding CategoryModel complete

--- src/main/java/com/alkemy/ong/repository/model/CategoryModel.java
+++ src/main/java/com/alkemy/ong/repository/model/CategoryModel.java
@@ -1,15 +1,25 @@
 package com.alkemy.ong.repository.model;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="categories")
 public class CategoryModel {
 
@@ -18,9 +28,13 @@ public class CategoryModel {
     private Long id;
     @Column(nullable = false)
     private String name;
-    @Column
     private String description;
-    @Column
     private String image;
+    @Column(name="creation_date")
+    private LocalDateTime creationDate;
 
+    @PrePersist
+    private void beforePersisting(){
+        this.creationDate = LocalDateTime.now();
+    }
 }
