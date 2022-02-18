22b38af2a81e53cde4a97e54219d72a08b70db1c
PabloD97 <pablo97758@gmail.com>
2022-02-18 11:10:20 -0300

    OT148-24: UserDTO
    - Added @NotBlank validation
    - Added Serializable interface

--- src/main/java/com/alkemy/ong/dto/UserDTO.java
+++ src/main/java/com/alkemy/ong/dto/UserDTO.java
@@ -3,19 +3,31 @@ package com.alkemy.ong.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Data
 @Builder
-public class UserDTO {
+public class UserDTO implements Serializable {
 
     @NotNull
+    @NotBlank
     private String name;
     @NotNull
+    @NotBlank
     private String lastname;
     @NotNull
+    @NotBlank
     private String email;
     @NotNull
+    @NotBlank
     private String password;
 
+    public UserDTO(String name, String lastname, String email, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
 }
