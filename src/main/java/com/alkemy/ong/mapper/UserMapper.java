beb6059e268107c8d0e0269b3d68fbb254400a3e
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-18 12:34:56 -0300

    OT148-17: UserMapper created

---
+++ src/main/java/com/alkemy/ong/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.alkemy.ong.mapper;
+
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.repository.model.UserModel;
+
+public class UserMapper {
+    public static User mapModelToDomain(UserModel userModel){
+        User userDomain = User.builder().
+                firstName(userModel.getFirstName()).
+                lastName(userModel.getLastName()).
+                email(userModel.getEmail()).
+                password(userModel.getPassword()).
+                photo(userModel.getPhoto()).
+                roleId(userModel.getRoleId()).build();
+        return userDomain;
+    }
+    public static UserModel mapDomainToModel(User userDomain){
+        UserModel userModel = UserModel.builder().
+                firstName(userDomain.getFirstName()).
+                lastName(userDomain.getLastName()).
+                email(userDomain.getEmail()).
+                password(userDomain.getPassword()).
+                photo(userDomain.getPhoto()).
+                roleId(userDomain.getRoleId()).build();
+        return userModel;
+    }
+}
