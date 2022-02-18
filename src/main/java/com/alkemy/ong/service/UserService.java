979dfb3553ff8d0467268d168968f96122fe8f8f
PabloD97 <pablo97758@gmail.com>
2022-02-17 22:28:05 -0300

    OT148-24: UserService
    - The password of the user is encrypted
    - Added registerUser() method

---
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -0,0 +1,38 @@
+package com.alkemy.ong.service;
+
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.mapper.UserMapper;
+import com.alkemy.ong.repository.UserRepository;
+import com.alkemy.ong.repository.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private UserMapper mapper;
+
+
+    @Transactional
+    public UserDTO registerUser(User user) {
+        UserModel userModel = mapper.mapDomainToModel(user);
+        userModel.setPassword(encryptPassword(user));
+        UserModel save = userRepository.save(userModel);
+        return mapper.mapModelToDto(save);
+    }
+
+    private String encryptPassword(User user){
+        String password = user.getPassword();
+        String encryptedPassword = passwordEncoder.encode(password);
+        return encryptedPassword;
+    }
+}
