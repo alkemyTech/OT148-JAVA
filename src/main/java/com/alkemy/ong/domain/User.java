a8b1cddcd4467d0ba7e6e6495242d62beeb39810
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-18 11:30:37 -0300

    OT148-17: User Domain created

---
+++ src/main/java/com/alkemy/ong/domain/User.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String photo;
+    private Role roleId;
+    private LocalDateTime creationDate;
+}
