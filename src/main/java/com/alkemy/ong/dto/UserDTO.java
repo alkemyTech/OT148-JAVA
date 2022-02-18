d1c0c231b53b9f0b69af50cb8a5e495806467f91
PabloD97 <pablo97758@gmail.com>
2022-02-18 13:28:02 -0300

    OT148-24: UserDTO
    - Added UserDTO

---
+++ src/main/java/com/alkemy/ong/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserDTO implements Serializable {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private String photo;
+    private LocalDateTime creationDate;
+}
