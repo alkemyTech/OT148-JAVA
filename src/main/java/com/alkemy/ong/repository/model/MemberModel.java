3b6bb3ee6bed92a9649f1a7c5e25d4266f2fbb82
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-18 17:26:25 -0300

    delete white spaces and add import class that are used

--- src/main/java/com/alkemy/ong/repository/model/MemberModel.java
+++ src/main/java/com/alkemy/ong/repository/model/MemberModel.java
@@ -1,27 +1,56 @@
 package com.alkemy.ong.repository.model;
 
+import lombok.Builder;
 import lombok.Data;
-import javax.persistence.*;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
 @Data
+@Builder
+@Entity
 @Table(name="members")
 public class MemberModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_member",nullable = false)
-    private Integer idMember;
+    private Long idMember;
     @Column(nullable = false)
     private String name;
-    @Column(name="facebook_url", nullable = true)
+    @Column(name="facebook_url")
     private String facebookUrl;
-    @Column(name="instagram_url", nullable = true)
+    @Column(name="instagram_url")
     private String instagramUrl;
-    @Column(name="linkedin_url", nullable = true )
+    @Column(name="linkedin_url")
     private String linkedinUrl;
     @Column(nullable = false)
     private String image;
-    @Column(nullable = true)
+    @Column
     private String description;
+
+    public MemberModel(Long idMember, String name, String facebookUrl, String instagramUrl, String linkedinUrl, String image, String description) {
+        this.idMember = idMember;
+        this.name = name;
+        this.facebookUrl = facebookUrl;
+        this.instagramUrl = instagramUrl;
+        this.linkedinUrl = linkedinUrl;
+        this.image = image;
+        this.description = description;
+    }
+
+    public MemberModel(String name, String facebookUrl, String instagramUrl, String linkedinUrl, String image, String description) {
+        this.name = name;
+        this.facebookUrl = facebookUrl;
+        this.instagramUrl = instagramUrl;
+        this.linkedinUrl = linkedinUrl;
+        this.image = image;
+        this.description = description;
+    }
+
+    public MemberModel() {}
 }
