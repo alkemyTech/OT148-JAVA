bd03fcfe3af79e8a8d9e6f6737c64329d38a9cc4
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-18 15:05:36 -0300

    OT148-17: Fixed UserMapper methods formating and punctuation

--- src/main/java/com/alkemy/ong/mapper/UserMapper.java
+++ src/main/java/com/alkemy/ong/mapper/UserMapper.java
@@ -5,23 +5,24 @@ import com.alkemy.ong.repository.model.UserModel;
 
 public class UserMapper {
     public static User mapModelToDomain(UserModel userModel){
-        User userDomain = User.builder().
-                firstName(userModel.getFirstName()).
-                lastName(userModel.getLastName()).
-                email(userModel.getEmail()).
-                password(userModel.getPassword()).
-                photo(userModel.getPhoto()).
-                role(userModel.getRole()).build();
+        User userDomain = User.builder()
+                .firstName(userModel.getFirstName())
+                .lastName(userModel.getLastName())
+                .email(userModel.getEmail())
+                .password(userModel.getPassword())
+                .photo(userModel.getPhoto())
+                .role(userModel.getRole()).build();
         return userDomain;
     }
+    
     public static UserModel mapDomainToModel(User userDomain){
-        UserModel userModel = UserModel.builder().
-                firstName(userDomain.getFirstName()).
-                lastName(userDomain.getLastName()).
-                email(userDomain.getEmail()).
-                password(userDomain.getPassword()).
-                photo(userDomain.getPhoto()).
-                role(userDomain.getRole()).build();
+        UserModel userModel = UserModel.builder()
+                .firstName(userDomain.getFirstName())
+                .lastName(userDomain.getLastName())
+                .email(userDomain.getEmail())
+                .password(userDomain.getPassword())
+                .photo(userDomain.getPhoto())
+                .role(userDomain.getRole()).build();
         return userModel;
     }
 }
