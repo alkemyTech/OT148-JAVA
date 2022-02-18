82645edf263d849f6e5d6beca87489839e38a7bc
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-18 09:49:38 -0300

    OT148-16 Rename RoleMapper methods

--- src/main/java/com/alkemy/ong/mapper/RoleMapper.java
+++ src/main/java/com/alkemy/ong/mapper/RoleMapper.java
@@ -4,13 +4,13 @@ import com.alkemy.ong.domain.Role;
 import com.alkemy.ong.repository.model.RoleModel;
 
 public class RoleMapper {
-    public static Role RoleModelToDomain(RoleModel roleModel){
+    public static Role mapModelToDomain(RoleModel roleModel){
         Role roleDomain = new Role();
         roleDomain.setName(roleModel.getName());
         roleDomain.setDescription(roleModel.getDescription());
         return roleDomain;
     }
-    public static RoleModel RoleDomainToModel (Role roleDomain){
+    public static RoleModel mapDomainToModel (Role roleDomain){
         RoleModel roleModel = new RoleModel();
         roleModel.setName(roleDomain.getName());
         roleModel.setDescription(roleDomain.getDescription());
