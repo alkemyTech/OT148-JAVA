a6afdc53c957b72c829d0afe8e2c7eb149fd8a44
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-18 10:25:38 -0300

    OT148-16 Changed import wildcard

--- src/main/java/com/alkemy/ong/repository/model/RoleModel.java
+++ src/main/java/com/alkemy/ong/repository/model/RoleModel.java
@@ -3,7 +3,12 @@ package com.alkemy.ong.repository.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data
