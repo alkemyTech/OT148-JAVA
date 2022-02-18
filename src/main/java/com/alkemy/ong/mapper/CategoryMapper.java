8541fadf2573fcdd489111c58b823e380883e89a
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-18 12:16:55 -0300

    Adding CategoryMapper class

--- src/main/java/com/alkemy/ong/mapper/CategoryMapper.java
+++ src/main/java/com/alkemy/ong/mapper/CategoryMapper.java
@@ -1,4 +1,28 @@
 package com.alkemy.ong.mapper;
 
+import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.repository.model.CategoryModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CategoryMapper {
+
+    public Category categoryModel2Category (CategoryModel categoryModel){
+        Category category= Category.builder().id(categoryModel.getId()).
+                name(categoryModel.getName()).
+                description(categoryModel.getDescription()).
+                image(categoryModel.getImage()).
+                creationDate(categoryModel.getCreationDate()).build();
+        return category;
+    }
+
+    public CategoryModel category2CategoryModel(Category category){
+        CategoryModel categoryModel= CategoryModel.builder().
+                id(category.getId()).
+                name(category.getName()).
+                description(category.getDescription()).
+                image(category.getImage()).
+                creationDate(category.getCreationDate()).build();
+        return categoryModel;
+    }
 }
--- src/main/java/com/alkemy/ong/repository/model/CategoryModel.java
+++ src/main/java/com/alkemy/ong/repository/model/CategoryModel.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.repository.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name="categories")
 public class CategoryModel {
 
