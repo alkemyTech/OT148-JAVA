03db3ab44dfef56ad4cf435686ae4809616b2553
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-18 11:57:31 -0300

    Annotation added in CategoryRepository class and adding Category domain class complete

--- src/main/java/com/alkemy/ong/domain/Category.java
+++ src/main/java/com/alkemy/ong/domain/Category.java
@@ -1,4 +1,14 @@
 package com.alkemy.ong.domain;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class Category {
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+    private LocalDateTime creationDate;
 }
--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.CategoryModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<CategoryModel,Long> {
 }
