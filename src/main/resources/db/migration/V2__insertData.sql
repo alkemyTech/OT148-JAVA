253e515abfe6c1dda7fdc54414c58b3a5728e3a7
PabloD97 <pablo97758@gmail.com>
2022-02-19 18:02:54 -0300

    OT148-24: db migration
    - Added the file .sql for to insert the roles

---
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO ROLES (name, description, creation_date) values
+                                        ('Administrator', 'Administrator Role', NOW()),
+                                        ('Normal', 'Normal Role', NOW());
