685bf8b734c14ad39c3603ccbcd744791f7366d9
PabloD97 <pablo97758@gmail.com>
2022-02-19 18:05:34 -0300

    OT148-24: UserMapper
    - remaining fields are added

--- src/main/java/com/alkemy/ong/mapper/UserMapper.java
+++ src/main/java/com/alkemy/ong/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.mapper;
 import com.alkemy.ong.domain.User;
 import com.alkemy.ong.dto.UserCreationDTO;
 import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.repository.model.RoleModel;
 import com.alkemy.ong.repository.model.UserModel;
 
 public class UserMapper {
@@ -10,28 +11,17 @@ public class UserMapper {
     public static User mapDtoCreationToDomain(UserCreationDTO userCreationDTO){
         User userDomain = User.builder().
                 email(userCreationDTO.getEmail()).
-                name(userCreationDTO.getName()).
-                lastname(userCreationDTO.getLastname()).
+                firstName(userCreationDTO.getName()).
+                lastName(userCreationDTO.getLastname()).
                 password(userCreationDTO.getPassword()).build();
         return userDomain;
     }
 
-    public static UserCreationDTO mapDomianToDtoCreation(User user){
-        UserCreationDTO userCreationDTO = UserCreationDTO.builder().
-                email(user.getEmail()).
-                name(user.getName()).
-                lastname(user.getLastname()).
-                password(user.getPassword()).build();
-        return userCreationDTO;
-    }
-
-
-
     public static UserCreationDTO mapModelToDtoCreation(UserModel user){
         UserCreationDTO userCreationDTO = UserCreationDTO.builder().
                 email(user.getEmail()).
-                name(user.getName()).
-                lastname(user.getLastname()).
+                name(user.getFirstName()).
+                lastname(user.getLastName()).
                 password(user.getPassword()).build();
         return userCreationDTO;
     }
@@ -39,12 +29,12 @@ public class UserMapper {
     public static UserDTO mapModelToDto(UserModel user){
         UserDTO userDTO = UserDTO.builder().
                 email(user.getEmail()).
-                firstName(user.getName()).
-                lastName(user.getLastname()).
-                creationDate(null).
+                firstName(user.getFirstName()).
+                lastName(user.getLastName()).
+                creationDate(user.getCreationDate()).
                 id(user.getId()).
-                password(null).
-                photo(null).build();
+                role(user.getRole()).
+                photo(user.getPhoto()).build();
         return userDTO;
     }
 
@@ -55,7 +45,7 @@ public class UserMapper {
                 .email(userModel.getEmail())
                 .password(userModel.getPassword())
                 .photo(userModel.getPhoto())
-                .role(userModel.getRole()).build();
+                .role(RoleMapper.mapModelToDomain(userModel.getRole())).build();
         return userDomain;
     }
 
@@ -66,7 +56,7 @@ public class UserMapper {
                 .email(userDomain.getEmail())
                 .password(userDomain.getPassword())
                 .photo(userDomain.getPhoto())
-                .role(userDomain.getRole()).build();
+                .role(RoleMapper.mapDomainToModel(userDomain.getRole())).build();
         return userModel;
     }
 }
