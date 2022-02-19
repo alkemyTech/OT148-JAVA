1c326b31074c21eca5d272131f24b2d98e09b978
PabloD97 <pablo97758@gmail.com>
2022-02-19 17:55:09 -0300

    OT148-24: UserModel
    - Added the cascade type MERGE to @ManyToOne

--- src/main/java/com/alkemy/ong/repository/model/UserModel.java
+++ src/main/java/com/alkemy/ong/repository/model/UserModel.java
@@ -8,15 +8,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Table;
-import javax.persistence.PrePersist;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -40,7 +32,7 @@ public class UserModel {
     @Column(nullable = false)
     private String password;
     private String photo;
-    @ManyToOne
+    @ManyToOne(cascade = {CascadeType.MERGE})
     @JoinColumn(name = "role_id",insertable = false, updatable = false)
     private RoleModel role;
     @Column(name = "creation_date")
