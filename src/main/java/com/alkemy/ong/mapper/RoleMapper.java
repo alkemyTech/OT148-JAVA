83dea8e17ea50c62f5b5591a80abcbb9653e463f
PabloD97 <pablo97758@gmail.com>
2022-02-19 18:06:15 -0300

    OT148-24: RoleMapper
    - remaining fields are added

--- src/main/java/com/alkemy/ong/mapper/RoleMapper.java
+++ src/main/java/com/alkemy/ong/mapper/RoleMapper.java
@@ -6,14 +6,18 @@ import com.alkemy.ong.repository.model.RoleModel;
 public class RoleMapper {
     public static Role mapModelToDomain(RoleModel roleModel){
         Role roleDomain = Role.builder().
+                id(roleModel.getId()).
                 name(roleModel.getName()).
-                description(roleModel.getDescription()).build();
+                description(roleModel.getDescription()).
+                creationDate(roleModel.getCreationDate()).build();
         return roleDomain;
     }
     public static RoleModel mapDomainToModel (Role roleDomain){
         RoleModel roleModel = RoleModel.builder().
+                id(roleDomain.getId()).
                 name(roleDomain.getName()).
-                description(roleDomain.getDescription()).build();
+                description(roleDomain.getDescription()).
+                creationDate(roleDomain.getCreationDate()).build();
         return roleModel;
     }
 }
