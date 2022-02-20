921e5fe666cda7c7a7e5369b660e9b61f8ccb10d
PabloD97 <pablo97758@gmail.com>
2022-02-20 15:28:09 -0300

    OT148-35: UserMapper
    - Added the method mapDomainToDTO()

--- src/main/java/com/alkemy/ong/mapper/UserMapper.java
+++ src/main/java/com/alkemy/ong/mapper/UserMapper.java
@@ -16,27 +16,6 @@ public class UserMapper {
         return userDomain;
     }
 
-    public static UserCreationDTO mapModelToDtoCreation(UserModel user){
-        UserCreationDTO userCreationDTO = UserCreationDTO.builder().
-                email(user.getEmail()).
-                name(user.getFirstName()).
-                lastName(user.getLastName()).
-                password(user.getPassword()).build();
-        return userCreationDTO;
-    }
-
-    public static UserDTO mapModelToDto(UserModel user){
-        User toDomain = mapModelToDomain(user);
-        UserDTO userDTO = UserDTO.builder().
-                email(toDomain.getEmail()).
-                firstName(toDomain.getFirstName()).
-                lastName(toDomain.getLastName()).
-                creationDate(toDomain.getCreationDate()).
-                id(toDomain.getId()).
-                photo(toDomain.getPhoto()).build();
-        return userDTO;
-    }
-
     public static User mapModelToDomain(UserModel userModel){
         User userDomain = User.builder()
                 .firstName(userModel.getFirstName())
@@ -57,5 +36,16 @@ public class UserMapper {
                 .photo(userDomain.getPhoto())
                 .role(RoleMapper.mapDomainToModel(userDomain.getRole())).build();
         return userModel;
+    }
+
+    public static UserDTO mapDomainToDTO(User userDomain){
+        UserDTO userDTO = UserDTO.builder().
+                lastName(userDomain.getLastName()).
+                firstName(userDomain.getFirstName()).
+                id(userDomain.getId()).
+                creationDate(userDomain.getCreationDate()).
+                email(userDomain.getEmail()).
+                photo(userDomain.getPhoto()).build();
+        return userDTO;
     }
 }
