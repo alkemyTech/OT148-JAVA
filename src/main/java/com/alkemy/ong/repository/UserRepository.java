c60346d3548dc9b2a82648af99f3740a27371634
PabloD97 <pablo97758@gmail.com>
2022-02-20 15:32:01 -0300

    OT148-35: UserRepository
    - Added the findAll() method that returns List<UserModel>

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.alkemy.ong.repository.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserModel,Long> {
+
+    List<UserModel> findAll();
 }
