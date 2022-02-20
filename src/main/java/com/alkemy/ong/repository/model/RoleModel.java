956b2fd4c33881d7994835339fff612cab3b1dff
PabloD97 <pablo97758@gmail.com>
2022-02-19 22:18:42 -0300

    OT148-35: Fixed imports

--- src/main/java/com/alkemy/ong/repository/model/RoleModel.java
+++ src/main/java/com/alkemy/ong/repository/model/RoleModel.java
@@ -5,9 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Data
 @Builder
--- src/main/java/com/alkemy/ong/repository/model/UserModel.java
+++ src/main/java/com/alkemy/ong/repository/model/UserModel.java
@@ -8,7 +8,16 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.PrePersist;
 import java.time.LocalDateTime;
 
 @Data
