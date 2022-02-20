4f0060f99ea2f4b6bf65bc4abff13fa95789757a
PabloD97 <pablo97758@gmail.com>
2022-02-19 22:17:47 -0300

    OT148-35: insertData.sql
    - Change the name from Administrator to ADMIN
    - Change the name from Normal to USER

--- src/main/resources/db/migration/V2__insertData.sql
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -1,3 +1,3 @@
 INSERT INTO ROLES (name, description, creation_date) values
-                                        ('Administrator', 'Administrator Role', NOW()),
-                                        ('Normal', 'Normal Role', NOW());
+                                        ('ADMIN', 'Administrator Role', NOW()),
+                                        ('USER', 'User Role', NOW());
