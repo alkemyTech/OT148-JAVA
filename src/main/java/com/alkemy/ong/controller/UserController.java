b8b2906473c446d9d5059de6c23e1c23fd982942
PabloD97 <pablo97758@gmail.com>
2022-02-19 21:49:35 -0300

    OT148-35: UserController
    - Added the endpoint '/users/

---
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.dto.UserCreationDTO;
+import com.alkemy.ong.mapper.UserMapper;
+import com.alkemy.ong.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<?> userRegister(@Valid @RequestBody UserCreationDTO userCreationDto){
+        User userDomain = UserMapper.mapDtoCreationToDomain(userCreationDto);
+        return ResponseEntity.ok(userService.registerUser(userDomain));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(userService.getAll());
+    }
+}
