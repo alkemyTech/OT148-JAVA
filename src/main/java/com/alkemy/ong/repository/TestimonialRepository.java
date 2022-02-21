9fff058748a4f7ad420bfa13290d43a921f38b88
Gabriel Cappelletti <gabrielk22@gmail.com>
2022-02-21 16:51:22 -0300

    Testimonial Repository

---
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.repository.model.TestimonialModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface TestimonialRepository extends CrudRepository<TestimonialRepository,Long> {
+    }
+
