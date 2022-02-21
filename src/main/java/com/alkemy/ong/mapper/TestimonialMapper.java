dd441b372240a7f8019ed1df2829e2dc5fb4b51d
Gabriel Cappelletti <gabrielk22@gmail.com>
2022-02-21 13:44:15 -0300

    Testimonial Mapper

---
+++ src/main/java/com/alkemy/ong/mapper/TestimonialMapper.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.mapper;
+
+import com.alkemy.ong.domain.Testimonial;
+import com.alkemy.ong.repository.model.TestimonialModel;
+
+public class TestimonialMapper {
+    public static Testimonial mapModelToDomain(TestimonialModel testimonialModel){
+        Testimonial testimonialDomain = Testimonial.builder().
+                name(testimonialModel.getName()).
+                image(testimonialModel.getImage()).build();
+                content(testimonialModel.getContent()).build();
+        return testimonialDomain;
+    }
+    public static TestimonialModel mapDomainToModel (Testimonial testimonialDomain){
+        TestimonialModel testimonialModel = TestimonialModel.builder().
+                name(testimonialDomain.getName()).
+                image(testimonialDomain.getImage()).build();
+                content(testimonialDomain.getContent()).build();
+        return testimonialModel;
+    }
+}
