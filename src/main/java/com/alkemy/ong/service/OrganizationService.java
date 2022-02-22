cd1477ae221c494b4f47c446ea8ffb50463d1f84
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-22 13:11:21 -0300

    OT148-23: Create OrganizationService

---
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.service;
+
+import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.mapper.OrganizationMapper;
+import com.alkemy.ong.repository.OrganizationRepository;
+import com.alkemy.ong.repository.model.OrganizationModel;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrganizationService {
+
+    private OrganizationRepository organizationRepository;
+
+    public OrganizationService (OrganizationRepository organizationRepository){
+        this.organizationRepository = organizationRepository;
+    }
+
+    @Transactional
+    public List<OrganizationDTO> findAll(){
+        List<OrganizationModel> organizationModelList = organizationRepository.findAll();
+        return organizationModelList.stream().map(OrganizationMapper::mapModelToDomain)
+                .map(OrganizationMapper::mapDomainToDTO)
+                .collect(Collectors.toList());
+    }
+}
