ec27580f68fe15bc45cae2a41aeb00a2b0c84bad
Gabriel Cappelletti <gabrielk22@gmail.com>
2022-02-22 10:48:00 -0300

    Modify Testimonial Repository

--- src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -4,7 +4,7 @@ import com.alkemy.ong.repository.model.TestimonialModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-    @Repository
-    public interface TestimonialRepository extends CrudRepository<TestimonialRepository,Long> {
-    }
+@Repository
+public interface TestimonialRepository extends CrudRepository<TestimonialModel, Long> {
+}
 
