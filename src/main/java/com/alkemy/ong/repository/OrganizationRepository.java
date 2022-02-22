4b8cba3745715e9d32207ef2335cd89fab6f0b22
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-22 13:13:46 -0300

    OT148-23: Add method findAll to OrganizationRepository

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -4,6 +4,9 @@ import com.alkemy.ong.repository.model.OrganizationModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrganizationRepository extends CrudRepository<OrganizationModel, Long> {
+    List<OrganizationModel> findAll();
 }
