c80529ee03ad461e591385ef90a40198d250f1b1
Gabriel Cappelletti <gabrielk22@gmail.com>
2022-02-22 10:43:02 -0300

    Modify Testimonial Domain

--- src/main/java/com/alkemy/ong/domain/Testimonial.java
+++ src/main/java/com/alkemy/ong/domain/Testimonial.java
@@ -5,13 +5,12 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-    @Data
-    @Builder
-    public class Testimonial {
-        private Long id;
-        private String name;
-        private String image;
-        private String content;
-        private LocalDateTime creationDate;
-    }
+@Data
+@Builder
+public class Testimonial {
+    private Long id;
+    private String name;
+    private String image;
+    private String content;
+    private LocalDateTime creationDate;
 }
