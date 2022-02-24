5396dafdd9f490c75fc409e8e89c17fd53ca9cee
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-24 18:04:27 -0300

    OT148-42: Created CategoryCreationDTO

---
+++ src/main/java/com/alkemy/ong/dto/CategoryCreationDTO.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.dto;
+
+import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CategoryCreationDTO implements Serializable {
+
+    @NotNull
+    @NotBlank
+    private String name;
+}
