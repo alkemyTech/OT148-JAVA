bab68c8f0e5e1eb4a2c27f0425c4f766c1577d0e
PabloD97 <pablo97758@gmail.com>
2022-02-23 21:07:05 -0300

    OT148-37: Set stage
    - Added the table ORGANIZATIONS to V1__init.sql
    - Organizations were inserted by V2__insertData.sql
    - Added the url 'organization/**' in SecurityConfig to test the endpoints

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,3 +5,16 @@ CREATE TABLE ROLES (
     creation_date VARCHAR (128),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE ORGANIZATIONS (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR (128) NOT NULL,
+    image VARCHAR (128) NOT NULL,
+    address VARCHAR (128) ,
+    phone INTEGER,
+    email VARCHAR (128) NOT NULL UNIQUE,
+    welcome_text VARCHAR (128) NOT NULL,
+    about_us_text VARCHAR (128),
+    creation_date VARCHAR (128),
+    PRIMARY KEY (id)
+);
