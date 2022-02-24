c94def1709163ce39a50d428ab5ca1e6b3a1dca9
Sebastian Vargas <vargaszsebastian@gmail.com>
2022-02-24 12:30:35 -0300

    JWT logic

--- src/main/java/com/alkemy/ong/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/security/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.alkemy.ong.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
@@ -12,7 +20,8 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+
+  /*  @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
                 .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
@@ -23,4 +32,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
         http.csrf().disable().authorizeRequests().antMatchers("/auth/**", "/h2/**", "/users/**").permitAll().anyRequest().authenticated();
        }
+       */
+
+        @Autowired
+        UserDetailsServiceImpl userDetailsService;
+        @Autowired
+        JwtEntryPoint jwtEntryPoint;
+
+        @Bean
+        public JwtTokenFilter jwtTokenFilter(){
+            return new JwtTokenFilter();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest ().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(),UsernamePasswordAuthenticationFilter.class);
+
+    }
 }
