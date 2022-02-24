1a3483b5f45a043381c06ce0e991b76c0619559a
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-24 18:09:07 -0300

    OT148-42: Added mapDomainToDTO and mapDTOCreationToDomain methods in CategoryMapper

--- src/main/java/com/alkemy/ong/service/OrganizationService.java
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -4,22 +4,20 @@ import com.alkemy.ong.dto.OrganizationDTO;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.model.OrganizationModel;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 
 public class OrganizationService {
 
-    private OrganizationRepository organizationRepository;
+    private final OrganizationRepository organizationRepository;
 
-    public OrganizationService (OrganizationRepository organizationRepository){
+    public OrganizationService(OrganizationRepository organizationRepository) {
         this.organizationRepository = organizationRepository;
     }
 
     @Transactional
-    public List<OrganizationDTO> findAll(){
+    public List<OrganizationDTO> findAll() {
         List<OrganizationModel> organizationModelList = organizationRepository.findAll();
         return organizationModelList.stream().map(OrganizationMapper::mapModelToDomain)
                 .map(OrganizationMapper::mapDomainToDTO)
