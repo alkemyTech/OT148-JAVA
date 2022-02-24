ebdd3b32fff138b3a5b7e81293fa58b677f66ddc
PabloD97 <pablo97758@gmail.com>
2022-02-23 21:03:25 -0300

    OT148-37: Endpoint update organization
    - Changed attribute id the Integer to Long OrganizationModel
    - Added updateOrganization() method in OrganizationService
    - Added endpoint '/organization/{id}' in OrganizationController
    - Added mapUpdateDTOToDomain() in OrganizationMapper
    - Added OrganizationNotFoundException
    - Added OrganizationUpdateDTO

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -1,24 +1,56 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.domain.Organization;
+import com.alkemy.ong.dto.ErrorDTO;
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.dto.OrganizationUpdateDTO;
+import com.alkemy.ong.exception.OrganizationNotFoundException;
+import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.service.OrganizationService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrganizationController {
 
     private OrganizationService organizationService;
 
-    public OrganizationController(OrganizationService organizationService){
+    public OrganizationController(OrganizationService organizationService) {
         this.organizationService = organizationService;
     }
 
     @GetMapping("/organization/public")
-    public ResponseEntity<List<OrganizationDTO>> getAll(){
+    public ResponseEntity<List<OrganizationDTO>> getAll() {
         return ResponseEntity.ok(organizationService.findAll());
     }
+
+    @PatchMapping("/organization/{id}")
+    public ResponseEntity<OrganizationDTO> updateOrganization(
+            @PathVariable Integer id, @Valid @RequestBody OrganizationUpdateDTO organizationUpdateDTO)
+            throws OrganizationNotFoundException {
+        Organization organizationDomain = OrganizationMapper
+                .mapUpdateDTOToDomain(organizationUpdateDTO);
+        OrganizationDTO organizationDTO =
+                OrganizationMapper.mapDomainToDTO(
+                        organizationService.updateOrganization(id, organizationDomain));
+        return ResponseEntity.ok(organizationDTO);
+    }
+
+    @ExceptionHandler(OrganizationNotFoundException.class)
+    private ResponseEntity<ErrorDTO> handleOrganizationNotFound(OrganizationNotFoundException ex) {
+        ErrorDTO organizationNotFound =
+                ErrorDTO.builder()
+                        .code(HttpStatus.NOT_FOUND)
+                        .message(ex.getMessage()).build();
+        return new ResponseEntity(organizationNotFound, HttpStatus.NOT_FOUND);
+    }
+
 }
--- src/main/java/com/alkemy/ong/service/OrganizationService.java
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -1,28 +1,48 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.domain.Organization;
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.exception.OrganizationNotFoundException;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.model.OrganizationModel;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 
 public class OrganizationService {
 
     private OrganizationRepository organizationRepository;
 
-    public OrganizationService (OrganizationRepository organizationRepository){
+    public OrganizationService(OrganizationRepository organizationRepository) {
         this.organizationRepository = organizationRepository;
     }
 
     @Transactional
-    public List<OrganizationDTO> findAll(){
+    public List<OrganizationDTO> findAll() {
         List<OrganizationModel> organizationModelList = organizationRepository.findAll();
         return organizationModelList.stream().map(OrganizationMapper::mapModelToDomain)
                 .map(OrganizationMapper::mapDomainToDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Organization updateOrganization(Integer id, Organization organization)
+            throws OrganizationNotFoundException {
+        if (organizationRepository.existsById(Long.valueOf(id))) {
+            OrganizationModel organizationOld = organizationRepository.findById(Long.valueOf(id)).get();
+            organizationOld.setAddress(organization.getAddress());
+            organizationOld.setAboutUsText(organization.getAboutUsText());
+            organizationOld.setCreationDate(organization.getCreationDate());
+            organizationOld.setEmail(organization.getEmail());
+            organizationOld.setImage(organization.getImage());
+            organizationOld.setName(organization.getName());
+            organizationOld.setPhone(organization.getPhone());
+            OrganizationModel updatedOrganization = organizationRepository.save(organizationOld);
+            return OrganizationMapper.mapModelToDomain(updatedOrganization);
+        } else {
+            throw new OrganizationNotFoundException(
+                    String.format("Organization with ID: %s not found", id));
+        }
     }
 }
