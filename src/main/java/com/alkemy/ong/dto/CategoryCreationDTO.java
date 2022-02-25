9bdf97187e104ad728051c7bb0491e2d9200c254
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-25 14:57:38 -0300

    OT148-42: Changes to CategoryCreationDTO variables

--- src/main/java/com/alkemy/ong/dto/CategoryCreationDTO.java
+++ src/main/java/com/alkemy/ong/dto/CategoryCreationDTO.java
@@ -14,14 +14,7 @@ public class CategoryCreationDTO implements Serializable {
 
     @NotNull
     @NotBlank
-    private Long id;
-    @NotNull
-    @NotBlank
     private String name;
-    @NotNull
-    @NotBlank
     private String description;
-    @NotNull
-    @NotBlank
     private String image;
 }
