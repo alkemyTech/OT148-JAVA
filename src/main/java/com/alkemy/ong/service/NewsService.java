25c2f8988ca15bbd0d0adca8b9a933ecf17de278
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-25 12:59:50 -0300

    adding method in NewsService class

--- src/main/java/com/alkemy/ong/service/NewsService.java
+++ src/main/java/com/alkemy/ong/service/NewsService.java
@@ -1,7 +1,12 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.NewsDTO;
+import com.alkemy.ong.mapper.NewsMapper;
 import com.alkemy.ong.repository.NewsRepository;
+import com.alkemy.ong.repository.model.NewsModel;
+import javassist.NotFoundException;
+
+import java.util.Optional;
 
 public class NewsService {
 
@@ -11,7 +16,13 @@ public class NewsService {
         this.newsRepository = newsRepository;
     }
 
-    public NewsDTO getById(Long id){
-
+    public NewsDTO getById(Long id) throws NotFoundException {
+        Optional<NewsModel> modelOptional = newsRepository.findById(id);
+        if(modelOptional.isPresent()){
+            NewsModel newsModel= modelOptional.get();
+            return NewsMapper.mapDomainToDTO(NewsMapper.mapModelToDomain(newsModel));
+        }else{
+            throw new NotFoundException(String.format("News with ID: %s not found", id));
+        }
     }
 }
