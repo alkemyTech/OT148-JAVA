82210fdbfc1c0d7ce828882f9283bd587cabd828
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-25 17:52:13 -0300

    Fixes in exception classes

--- src/main/java/com/alkemy/ong/exception/NewsNotFoundException.java
+++ src/main/java/com/alkemy/ong/exception/NewsNotFoundException.java
@@ -2,7 +2,7 @@ package com.alkemy.ong.exception;
 
 import javassist.NotFoundException;
 
-public class NewsNotFoundException extends NotFoundException {
+public class NewsNotFoundException extends RuntimeException {
 
     public NewsNotFoundException(String msg) {
         super(msg);
