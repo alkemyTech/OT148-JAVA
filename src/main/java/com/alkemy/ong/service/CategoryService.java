268baba82d8d04f9d028da2340ca307bf96250c6
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-26 14:02:06 -0300

    OT148-41: Added method getById in CategoryService

--- src/main/java/com/alkemy/ong/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -2,10 +2,12 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.domain.Category;
 import com.alkemy.ong.dto.CategoryDTO;
+import com.alkemy.ong.exception.CategoryNotFoundException;
 import com.alkemy.ong.mapper.CategoryMapper;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.repository.model.CategoryModel;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 
@@ -30,5 +32,16 @@ public class CategoryService {
         return categoryModelList.stream().map(CategoryMapper::mapModelToDomain)
                 .map(CategoryMapper::mapDomainToDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Category getById(Long id) throws CategoryNotFoundException {
+        Optional<CategoryModel> modelOptional = categoryRepository.findById(id);
+        if (!modelOptional.isEmpty()) {
+            CategoryModel categoryModel = modelOptional.get();
+            return CategoryMapper.mapModelToDomain(categoryModel);
+        } else {
+            throw new CategoryNotFoundException(String.format("Categories with ID: %s not found", id));
+        }
     }
 }
