3aadfeb9b7f7fab7215ef794bcf6b74aa7b232f1
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-26 14:03:02 -0300

    OT148-41: Added methods getById and handleCategoryNotFoundExceptions in CategoryController

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -3,12 +3,17 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.domain.Category;
 import com.alkemy.ong.dto.CategoryCreationDTO;
 import com.alkemy.ong.dto.CategoryDTO;
+import com.alkemy.ong.dto.ErrorDTO;
+import com.alkemy.ong.exception.CategoryNotFoundException;
 import com.alkemy.ong.mapper.CategoryMapper;
 import com.alkemy.ong.service.CategoryService;
 import java.util.List;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +37,18 @@ public class CategoryController {
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryDTO>> getAll() {
         return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryDTO> getById(@PathVariable Long id) throws CategoryNotFoundException {
+        return ResponseEntity.ok(CategoryMapper.mapDomainToDTO(categoryService.getById(id)));
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleCategoryNotFoundExceptions(CategoryNotFoundException ex) {
+        ErrorDTO categoryNotFound = ErrorDTO.builder()
+                .code(HttpStatus.NOT_FOUND)
+                .message(ex.getMessage()).build();
+        return new ResponseEntity(categoryNotFound, HttpStatus.NOT_FOUND);
     }
 }
