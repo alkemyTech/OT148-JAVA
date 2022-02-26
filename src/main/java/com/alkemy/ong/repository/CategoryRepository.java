2f2a42dd9e7bf61102ed4a977237aeb96a61b768
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-26 12:33:41 -0300

    OT148-40: Added findAll method in CategoryRepository

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.CategoryModel;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<CategoryModel,Long> {
+public interface CategoryRepository extends CrudRepository<CategoryModel, Long> {
+
+    List<CategoryModel> findAll();
 }
