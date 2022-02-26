bd6869b352b9c4df2349cebc4df2b1262ba97302
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-26 12:34:36 -0300

    OT148-40: Added getAll method in CategoryService

--- src/main/java/com/alkemy/ong/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.mapper.CategoryMapper;
 import com.alkemy.ong.repository.CategoryRepository;
 import com.alkemy.ong.repository.model.CategoryModel;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 
 public class CategoryService {
@@ -19,5 +22,13 @@ public class CategoryService {
         CategoryModel categoryModel = CategoryMapper.mapDomainToModel(category);
         CategoryModel save = categoryRepository.save(categoryModel);
         return CategoryMapper.mapModelToDomain(save);
+    }
+
+    @Transactional
+    public List<CategoryDTO> getAll() {
+        List<CategoryModel> categoryModelList = categoryRepository.findAll();
+        return categoryModelList.stream().map(CategoryMapper::mapModelToDomain)
+                .map(CategoryMapper::mapDomainToDTO)
+                .collect(Collectors.toList());
     }
 }
