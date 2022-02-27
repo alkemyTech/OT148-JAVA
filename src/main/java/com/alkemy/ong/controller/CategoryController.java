ffa1a692fe2d46ac16005ba6ec21b7faa7c43224
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-02-27 16:56:55 -0300

    OT148-43: Added method updateCategory in CategoryController

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.domain.Category;
 import com.alkemy.ong.dto.CategoryCreationDTO;
 import com.alkemy.ong.dto.CategoryDTO;
+import com.alkemy.ong.dto.CategoryUpdateDTO;
 import com.alkemy.ong.dto.ErrorDTO;
 import com.alkemy.ong.exception.CategoryNotFoundException;
 import com.alkemy.ong.mapper.CategoryMapper;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -46,6 +49,13 @@ public class CategoryController {
     @GetMapping("/categories/{id}")
     public ResponseEntity<CategoryDTO> getById(@PathVariable Long id) throws CategoryNotFoundException {
         return ResponseEntity.ok(CategoryMapper.mapDomainToDTO(categoryService.getById(id)));
+    }
+
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id, @RequestPart("category") CategoryUpdateDTO categoryUpdateDTO) throws CategoryNotFoundException {
+        Category category = CategoryMapper.mapUpdateDTOToDomain(categoryUpdateDTO);
+        CategoryDTO categoryDTO = CategoryMapper.mapDomainToDTO(categoryService.updateCategory(id, category));
+        return ResponseEntity.ok(categoryDTO);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
