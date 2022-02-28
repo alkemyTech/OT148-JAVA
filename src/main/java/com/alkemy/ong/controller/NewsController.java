6335d044c3f1bd9cd7df6dbf9ad77957fc05b4d2
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-02-28 10:30:35 -0300

    Adding update method to NewsController class

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,14 @@ public class NewsController {
                         .message(ex.getMessage()).build();
         return new ResponseEntity(newsNotFound, HttpStatus.NOT_FOUND);
 
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<NewsDTO> updateNews(@PathVariable Long id,
+                                              @RequestBody NewsDTO newsDTO) throws NewsNotFoundException {
+        News news = NewsMapper.mapDTOToDomain(newsDTO);
+        NewsDTO newsUpdated = NewsMapper.mapDomainToDTO(newsService.updateNews(id, news));
+        return ResponseEntity.ok().body(newsUpdated);
     }
 
     @PostMapping
