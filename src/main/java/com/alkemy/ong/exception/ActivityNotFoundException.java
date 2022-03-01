1650848d227e057bd88553452323c99f2295198e
Nahuel <walternahuelbarrios@hotmail.com>
2022-02-28 21:24:07 -0300

    OT148-50-create class activitynotfoundexception

---
+++ src/main/java/com/alkemy/ong/exception/ActivityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.exception;
+
+public class ActivityNotFoundException extends RuntimeException {
+
+    public ActivityNotFoundException(String msg) {
+        super(msg);
+    }
+}
