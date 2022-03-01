ca2387456ae230a2e2a62ff694b6ede95e22df01
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-01 12:33:54 -0300

    OT148-50-Modify createActivity and add handlevalidationExceptions

--- src/main/java/com/alkemy/ong/controller/ActivityController.java
+++ src/main/java/com/alkemy/ong/controller/ActivityController.java
@@ -3,16 +3,24 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.domain.Activity;
 import com.alkemy.ong.dto.ActivityCreationDTO;
 import com.alkemy.ong.dto.ActivityDTO;
+import com.alkemy.ong.dto.ErrorDTO;
 import com.alkemy.ong.exception.ActivityNotFoundException;
 import com.alkemy.ong.mapper.ActivityMapper;
 import com.alkemy.ong.service.ActivityService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,11 +33,11 @@ public class ActivityController {
     }
 
     @PostMapping("/activities")
-    public ResponseEntity<ActivityDTO> createActivity(@Valid @RequestBody ActivityCreationDTO activityCreationDTO)
-            throws ActivityNotFoundException {
-        Activity activityDomain = ActivityMapper.mapCreationDTOToDomain(activityCreationDTO);
-        ActivityDTO activityDTO = ActivityMapper.mapDomainToDTO(activityService.createActivity(activityDomain));
-        return ResponseEntity.ok(activityDTO);
+    public ResponseEntity<ActivityDTO> createActivity(@Valid @RequestBody ActivityCreationDTO activityCreationDTO) throws ActivityNotFoundException {
+
+        Activity activity = activityService.createActivity(ActivityMapper.mapCreationDTOToDomain(activityCreationDTO));
+        ActivityDTO activityDTO = ActivityMapper.mapDomainToDTO(activity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(activityDTO);
     }
 
     @GetMapping("/activities")
@@ -38,5 +46,26 @@ public class ActivityController {
                 .stream().map(ActivityMapper::mapDomainToDTO)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(activityDTOS);
+    }
+
+    @ExceptionHandler(ActivityNotFoundException.class)
+    private ResponseEntity<ErrorDTO> handleActivityNotFound(ActivityNotFoundException ex) {
+        ErrorDTO activityNotFound =
+                ErrorDTO.builder()
+                        .code(HttpStatus.NOT_FOUND)
+                        .message(ex.getMessage()).build();
+        return new ResponseEntity(activityNotFound, HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
