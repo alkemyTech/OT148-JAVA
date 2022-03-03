ba5e4404608250cf81a6d918b49800c2367cf0e3
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-03 15:59:24 -0300

    [OT148-52] data overload activities

--- src/main/resources/db/migration/V2__insertData.sql
+++ src/main/resources/db/migration/V2__insertData.sql
@@ -5,3 +5,7 @@ INSERT INTO ROLES (name, description, creation_date) values
 INSERT INTO ORGANIZATIONS (name, image, address, phone, email, welcome_text, about_us_text ,creation_date) values
                                         ('Somos Mas', 'somos-mas.jpg', 'Direccion n', 1212323, 'somosmas@gmail.com', 'Welcome to Somos Mas', 'Somos una organizacion',NOW()),
                                         ('Unicef', 'unicef.jpg', 'Direccion u', 1212321, 'unicef@gmail.com', 'Welcome to Unicef', 'Somos una organizacion',NOW());
+
+INSERT INTO ACTIVITIES (name, content, image, deleted, creation_date) values
+                                        ('Actividades deportivas', 'Deporte para los niños','img.jpg','false',NOW()),
+                                        ('Colecta de alimentos', 'Colecta de ropa','img.jpg','false',NOW());
