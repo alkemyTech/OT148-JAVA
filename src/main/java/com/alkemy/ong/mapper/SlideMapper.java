5a32ced430c7bd25c4fc6ca86bac3ccf01d2d939
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-03 16:24:36 -0300

    OT148-57: Added methods mapDTOToDomain, mapDomainToDTO and mapCreationDTOToDomain in SlideMapper

--- src/main/java/com/alkemy/ong/mapper/SlideMapper.java
+++ src/main/java/com/alkemy/ong/mapper/SlideMapper.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.mapper;
 
 import com.alkemy.ong.domain.Slide;
+import com.alkemy.ong.dto.SlideCreationDTO;
+import com.alkemy.ong.dto.SlideDTO;
 import com.alkemy.ong.repository.model.SlideModel;
 
 public class SlideMapper {
@@ -20,5 +22,34 @@ public class SlideMapper {
                 .order(slideDomain.getOrder())
                 .organizationModel(OrganizationMapper.mapDomainToModel(slideDomain.getOrganization())).build();
         return slideModel;
+    }
+
+    public static Slide mapDTOToDomain(SlideDTO dto) {
+        Slide slide = Slide.builder()
+                .id(dto.getId())
+                .image(dto.getImage())
+                .text(dto.getText())
+                .order(dto.getOrder())
+                .build();
+        return slide;
+    }
+
+    public static SlideDTO mapDomainToDTO(Slide slideDomain) {
+        SlideDTO slideDTO = SlideDTO.builder()
+                .id(slideDomain.getId())
+                .image(slideDomain.getImage())
+                .text(slideDomain.getText())
+                .order(slideDomain.getOrder())
+                .build();
+        return slideDTO;
+    }
+
+    public static Slide mapCreationDTOToDomain(SlideCreationDTO slideCreationDTO) {
+        Slide slide = Slide.builder()
+                .image(slideCreationDTO.getImage())
+                .text(slideCreationDTO.getText())
+                .order(slideCreationDTO.getOrder())
+                .build();
+        return slide;
     }
 }
