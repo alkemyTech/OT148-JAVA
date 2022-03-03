0ac4276a0a7cb90ed031b2d71d8bb0563d2ced44
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-03 16:21:07 -0300

    OT148-57: Created class SlideDTO

---
+++ src/main/java/com/alkemy/ong/dto/SlideDTO.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SlideDTO implements Serializable {
+    private Long id;
+    private String image;
+    private String text;
+    private Integer order;
+}
