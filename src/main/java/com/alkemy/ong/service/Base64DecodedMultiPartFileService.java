57d41ba451bca3039426c814fb5ca2e0d9adf0dd
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-03 19:43:59 -0300

    OT148-57: Created class Base64DecodedMultiPartFileService

---
+++ src/main/java/com/alkemy/ong/service/Base64DecodedMultiPartFileService.java
@@ -0,0 +1,57 @@
+package com.alkemy.ong.service;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.web.multipart.MultipartFile;
+
+public class Base64DecodedMultiPartFileService implements MultipartFile {
+
+    private final byte[] imageContent;
+
+    public Base64DecodedMultiPartFileService(byte[] imageContent) {
+        this.imageContent = imageContent;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return imageContent == null || imageContent.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return imageContent.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return imageContent;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(imageContent);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        new FileOutputStream(dest).write(imageContent);
+    }
+}
