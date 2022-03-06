8dfdcd0683b5d1d9dd45cebdb8d19b0a24df7417
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-06 11:08:25 -0300

    OT148-57: Changes to method findFirstByOrganization_IdOrderByOrganizationOrderDesc in SlideRepository

--- src/main/java/com/alkemy/ong/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/repository/SlideRepository.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.SlideModel;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface SlideRepository extends CrudRepository<SlideModel, Long> {
     List<SlideModel> findByOrganization_IdOrderByOrganizationOrder(Long id);
 
-    SlideModel findFirstByOrganization_IdOrderByOrganizationOrderDesc(Long id);
+    Optional<SlideModel> findFirstByOrganization_IdOrderByOrganizationOrderDesc(Long id);
 }
