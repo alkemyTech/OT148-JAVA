e6c97d141dfcadddb0222b8726825508d6eb4d9a
Sebastian Vargas <vargaszsebastian@gmail.com>
2022-03-08 11:05:52 -0300

    Comment done

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -4,13 +4,19 @@ import com.alkemy.ong.domain.Comment;
 import com.alkemy.ong.dto.CommentDTO;
 import com.alkemy.ong.mapper.CommentMapper;
 import com.alkemy.ong.service.CommentService;
-import com.alkemy.ong.service.ContactService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -18,14 +24,26 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    public ContactController(ContactService contactService) {
-        this.contactService = contactService;
+    public CommentController (CommentService commentService) {
+        this.commentService = commentService;
     }
 
     @PostMapping("/comments")
     public ResponseEntity<CommentDTO> storeContact(@Valid @RequestBody CommentDTO commentDTO) {
         Comment comment = CommentMapper.mapDtoToDomain(commentDTO);
-        CommentDTO commentDTOSaved = CommentMapper.mapDomainToDto(commentService.addComment(comment);
+        CommentDTO commentDTOSaved = CommentMapper.mapDomainToDto(commentService.addComment(comment));
         return ResponseEntity.ok(commentDTOSaved);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- src/main/java/com/alkemy/ong/repository/CommentRepository.java
+++ src/main/java/com/alkemy/ong/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.alkemy.ong.repository;
 
+import com.alkemy.ong.repository.model.CommentModel;
 import com.alkemy.ong.repository.model.NewsModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository<NewsModel, Long> {
+public interface CommentRepository extends CrudRepository<CommentModel, Long> {
 }
