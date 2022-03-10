1d41b36560b27e1ef9c31efd9e0d5886b2696162
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-03-10 10:35:21 -0300

    Adding delete method in Member service class

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -9,8 +9,10 @@ import com.alkemy.ong.mapper.MemberMapper;
 import com.alkemy.ong.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +45,12 @@ public class MemberController {
         Member member = MemberMapper.mapCreationDTOToDomain(memberCreationDTO);
         MemberDTO memberDTO = MemberMapper.mapDomainToDTO(memberService.createMember(member));
         return ResponseEntity.status(HttpStatus.CREATED).body(memberDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMember(@PathVariable Long id) throws MemberNotFoundException {
+        memberService.deleteMember(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
