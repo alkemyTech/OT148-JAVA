d37aeef573f5fb54ef54d401e66d82c511228d41
PabloD97 <pablo97758@gmail.com>
2022-03-10 11:53:24 -0300

    OT148-84: Endpoint /api/docs

---
+++ src/main/java/com/alkemy/ong/controller/SwaggerController.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@ApiIgnore
+public class SwaggerController {
+
+    @RequestMapping("/api/docs")
+    public String greeting() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
