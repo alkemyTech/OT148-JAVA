5b85426d6a1604efcf39fd68565b9435aec4a339
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-12 16:00:44 -0300

    [OT148-80] modified CrudRepository to JpaRepository

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -1,12 +1,9 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.CategoryModel;
-import java.awt.print.Pageable;
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<CategoryModel, Long> {
-    List<CategoryModel> findAll(Pageable pageable);
+public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
 }
