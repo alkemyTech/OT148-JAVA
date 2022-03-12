64ea9200d197be05d06de3c6914a16fba433c804
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-12 16:05:28 -0300

    [OT148-80] modify to findAllPage with a size of 10 categories

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -11,6 +11,10 @@ import com.alkemy.ong.service.CategoryService;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CategoryController {
@@ -38,12 +43,24 @@ public class CategoryController {
         return ResponseEntity.ok(categoryDTO);
     }
 
-    @GetMapping(value = "/categories", params = "page")
-    public ResponseEntity<List<CategoryDTO>> getAll(@PathVariable("page") int pageNro) {
-        List<CategoryDTO> categoryDTOS = categoryService.getAll(pageNro)
-                .stream().map(CategoryMapper::mapDomainToDTO)
+    @GetMapping("/categories")
+    public ResponseEntity<?> findAllPage(Pageable pageable) {
+        int page = pageable.getPageNumber();
+        final int size = 10;
+        List<CategoryDTO> list = categoryService.findAllPage(PageRequest.of(page, size)).stream().map(CategoryMapper::mapDomainToDTO)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(categoryDTOS);
+        Page<CategoryDTO> pages = new PageImpl<CategoryDTO>(list, pageable, list.size());
+        if (page < 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()
+                            + "/categories/?page=0");
+        }
+        if (page >= 0 && (pages.getNumberOfElements() != 0)) {
+            return ResponseEntity.ok().body(pages);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()
+                        + "/categories/?page=" + pages.previousPageable().getPageNumber());
     }
 
     @GetMapping("/categories/{id}")
