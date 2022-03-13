230b10321bbf41f44e52c397903fac03bc71602c
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-13 20:28:18 -0300

    [OT148-80] modified method getCategories

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -5,16 +5,11 @@ import com.alkemy.ong.dto.CategoryCreationDTO;
 import com.alkemy.ong.dto.CategoryDTO;
 import com.alkemy.ong.dto.CategoryUpdateDTO;
 import com.alkemy.ong.dto.ErrorDTO;
+import com.alkemy.ong.dto.PageDTO;
 import com.alkemy.ong.exception.CategoryNotFoundException;
 import com.alkemy.ong.mapper.CategoryMapper;
 import com.alkemy.ong.service.CategoryService;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,8 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CategoryController {
@@ -43,24 +38,12 @@ public class CategoryController {
         return ResponseEntity.ok(categoryDTO);
     }
 
-    @GetMapping("/categories")
-    public ResponseEntity<?> findAllPage(Pageable pageable) {
-        int page = pageable.getPageNumber();
-        final int size = 10;
-        List<CategoryDTO> list = categoryService.findAllPage(PageRequest.of(page, size)).stream().map(CategoryMapper::mapDomainToDTO)
-                .collect(Collectors.toList());
-        Page<CategoryDTO> pages = new PageImpl<CategoryDTO>(list, pageable, list.size());
-        if (page < 0) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()
-                            + "/categories/?page=0");
-        }
-        if (page >= 0 && (pages.getNumberOfElements() != 0)) {
-            return ResponseEntity.ok().body(pages);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()
-                        + "/categories/?page=" + pages.previousPageable().getPageNumber());
+    @GetMapping(value = "/categories", params = "page")
+    public ResponseEntity<PageDTO<CategoryDTO>> getAllCategories(@RequestParam int page)
+            throws CategoryNotFoundException {
+        PageDTO<Category> category = categoryService.findAll(page);
+        PageDTO<CategoryDTO> categoryDTOPageDTO = CategoryMapper.mapPageDomainToPageDTO(category);
+        return ResponseEntity.ok(categoryDTOPageDTO);
     }
 
     @GetMapping("/categories/{id}")
