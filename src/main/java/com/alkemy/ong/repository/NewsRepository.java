41123b287fcbb3eeff4ceef734827bed1c4342ae
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-14 17:02:07 -0300

    OT148-81: Delete method findAll in NewsRepository

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,12 +1,9 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.NewsModel;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsModel, Long> {
-
-    List<NewsModel> findAll();
 }
