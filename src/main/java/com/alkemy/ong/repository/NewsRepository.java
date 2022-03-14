034317207765b9e1a99be64a5f90042435d3a990
Francisco Cotorruelo <franciscoctrrlo@gmail.com>
2022-03-14 12:01:05 -0300

    OT148-81: Changed CrudRepository extend to JpaRepository

--- src/main/java/com/alkemy/ong/repository/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/NewsRepository.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.repository.model.NewsModel;
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsRepository extends CrudRepository<NewsModel, Long> {
+public interface NewsRepository extends JpaRepository<NewsModel, Long> {
+
+    List<NewsModel> findAll();
 }
