dd29432f0babed58ba30227e9c8f4d26a1c145f3
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-14 12:22:54 -0300

    [OT148-80] add annotation JsonInclude in PageDTO

--- src/main/java/com/alkemy/ong/dto/PageDTO.java
+++ src/main/java/com/alkemy/ong/dto/PageDTO.java
@@ -1,9 +1,11 @@
 package com.alkemy.ong.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PageDTO<T> {
     private Integer totalPages;
     private String nextPage;
