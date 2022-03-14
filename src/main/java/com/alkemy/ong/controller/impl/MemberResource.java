28ce4cef0a11ffb3a4a1b5947b6287b74805253f
Tescelin16 <daniel.edg.cocco@gmail.com>
2022-03-14 18:00:00 -0300

    Fixing MemberController class

--- src/main/java/com/alkemy/ong/controller/impl/MemberResource.java
+++ src/main/java/com/alkemy/ong/controller/impl/MemberResource.java
@@ -13,21 +13,15 @@ import com.alkemy.ong.service.MemberService;
 import com.alkemy.ong.util.ContextUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/members")
 public class MemberResource implements MemberController {
 
     private final MemberService memberService;
@@ -36,27 +30,23 @@ public class MemberResource implements MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping
     public ResponseEntity<MemberListDTO> getAll(@RequestParam(defaultValue = "0") Integer page) {
         var members = memberService.getAll(page);
         MemberListDTO response = new MemberListDTO(page, members, ContextUtils.currentContextPath());
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/{id}")
     public ResponseEntity<MemberDTO> updateMember(@PathVariable Long id, @RequestBody MemberUpdateDTO memberUpdateDTO) throws MemberNotFoundException {
         Member member = MemberMapper.mapUpdateDTOToDomain(memberUpdateDTO);
         return ResponseEntity.ok(MemberMapper.mapDomainToDTO(memberService.updateMember(id, member)));
     }
 
-    @PostMapping
     public ResponseEntity<MemberDTO> createMember(@Valid @RequestBody MemberCreationDTO memberCreationDTO) {
         Member member = MemberMapper.mapCreationDTOToDomain(memberCreationDTO);
         MemberDTO memberDTO = MemberMapper.mapDomainToDTO(memberService.createMember(member));
         return ResponseEntity.status(HttpStatus.CREATED).body(memberDTO);
     }
 
-    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteMember(@PathVariable Long id) throws MemberNotFoundException {
         memberService.deleteMember(id);
         return new ResponseEntity(HttpStatus.OK);
