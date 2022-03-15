1b556d05494cd86b2865cfa8c36e1088f6bee8f6
Sebastian Vargas <vargaszsebastian@gmail.com>
2022-03-15 19:11:31 -0300

    character g eliminated

--- src/main/java/com/alkemy/ong/controller/impl/TestimonialResource.java
+++ src/main/java/com/alkemy/ong/controller/impl/TestimonialResource.java
@@ -16,7 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;g
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
