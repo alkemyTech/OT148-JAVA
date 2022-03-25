be3119d8fff6e07a281c9a0df6f0fcc319e4cedb
Sebastian Vargas <vargaszsebastian@gmail.com>
2022-03-25 16:11:39 -0300

    Slides Documentation

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,11 +1,20 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.JwtDTO;
 import com.alkemy.ong.dto.SlideCreationDTO;
 import com.alkemy.ong.dto.SlideDTO;
 import com.alkemy.ong.dto.SlideUpdateDTO;
+import com.alkemy.ong.dto.UserDTO;
 import com.alkemy.ong.exception.SlideNotFoundException;
 import java.util.List;
 import javax.validation.Valid;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,25 +24,79 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Tag(name = "Slides", description = "Operations related to Slides")
 public interface SlideController {
 
+    @Operation(
+            summary = "Find all slides in a page",
+            description = "To get a paginated list of the slides , you must access this endpoint"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "Find a page of slides",
+            content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class))
+            })
     @GetMapping("/slides/{id}")
     @ResponseStatus(HttpStatus.OK)
     SlideDTO slideDetails(@PathVariable("id") Long id) throws SlideNotFoundException;
 
+    @Operation(summary = "Delete a Slide by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Delete Slide by id",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Slide not found",
+                    content = @Content)})
     @DeleteMapping("/slides/{id}")
     @ResponseStatus(HttpStatus.OK)
     void deleteSlide(@PathVariable("id") Long id) throws SlideNotFoundException;
 
+    @Operation(summary = "Update Slide by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update Slide by id",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = SlideDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Slide not found",
+                    content = @Content)})
     @PutMapping("/slides/{id}")
     @ResponseStatus(HttpStatus.OK)
     SlideDTO updateSlide(@PathVariable Long id, @RequestBody SlideUpdateDTO slideUpdateDTO) throws SlideNotFoundException;
 
+
+    @Operation(
+            summary = "Register a new Slide",
+            description = "To register, this endpoint must be accessed"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Register Slide",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = JwtDTO.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "The fields must not be empty",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "This Slide must have an Organization",
+                    content = @Content)
+    })
     @PostMapping("/slides")
     @ResponseStatus(HttpStatus.CREATED)
     SlideDTO createSlide(@Valid @RequestBody SlideCreationDTO slideCreationDTO);
 
+    @Operation(
+            summary = "Find all slides",
+            description = "To fetch all slides, you must access this endpoint"
+    )
+    @ApiResponse(responseCode = "200",
+            description = "Find all slides",
+            content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = List.class))
+            })
     @GetMapping("/slides")
     @ResponseStatus(HttpStatus.OK)
     List<SlideDTO> findAll();
-}
+}g
