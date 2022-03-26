205d410f04e3f55ed077eb78a332637c961e2c79
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-26 11:03:14 -0300

    [OT148] add swagger in Comments

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -1,6 +1,11 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.ContactDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -9,12 +14,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Tag(name = "Contact", description = "Operations related to Contact")
 public interface ContactController {
 
+    @Operation(
+            summary = "Create contact",
+            description = "To create contact you must access this endpoint"
+
+    )
+    @ApiResponse(responseCode = "201",
+            description = "Create contact",
+            content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ContactDTO.class))
+            })
     @PostMapping("/contacts")
     @ResponseStatus(HttpStatus.CREATED)
     ContactDTO createContact(@Valid @RequestBody ContactDTO contactDTO);
 
+    @Operation(
+            summary = "Get all contacts",
+            description = "To get a list of news you must access this endpoint"
+
+    )
+    @ApiResponse(responseCode = "200",
+            description = "Get all contacts")
     @GetMapping("/contacts")
     List<ContactDTO> findAll();
 
